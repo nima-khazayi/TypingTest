7cdea55081b03dd57e0508b7794a02c278be9a17
Nima <Nimakhazayi7@gmail.com>
2025-07-20 21:22:47 +0330

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from initializer import root
-import shutil
-
-if __name__ == "__main__":
-    terminal_size = shutil.get_terminal_size()
-    height = terminal_size.lines
-    width = terminal_size.columns
-    root("Your text here", height, width)  # Example dimensions
